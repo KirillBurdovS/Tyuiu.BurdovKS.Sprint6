c9bdfb413d1bed8285a5c2a032541709b2494117
Кирилл Бурдов <Tyuiu.BurdovKS@outlook.com>
2024-12-10 17:00:58 +0300

    Task6

--- Tyuiu.BurdovKS.Sprint6.Task6.V25.Lib/DataService.cs
+++ Tyuiu.BurdovKS.Sprint6.Task6.V25.Lib/DataService.cs
@@ -14,13 +14,13 @@ namespace Tyuiu.BurdovKS.Sprint6.Task6.V25.Lib
     {
         public string CollectTextFromFile(string path)
         {
-            string resStr = "";
+             string resStr = "";
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    string[] words = line.Split((char)StringSplitOptions.RemoveEmptyEntries);
+                    string[] words = line.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
                     foreach (string word in words)
                     {
                         if (word.Contains("E"))
